bc2fd561ac4171a0b4dae4ef7fe4638990e46b79
Konstantina Boua <85695279+JackieTsakie@users.noreply.github.com>
2023-05-27 22:00:23 +0300

    Added current date and time of  locations

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,23 @@
+function updateTime() {
+  let madridElement = document.querySelector("#madrid");
+  let madridDateElement = madridElement.querySelector(".date");
+  let madridTimeElement = madridElement.querySelector(".time");
+  let madridTime = moment().tz("Europe/Madrid");
 
+  madridDateElement.innerHTML = madridTime.format("MMMM Do YYYY");
+  madridTimeElement.innerHTML = madridTime.format(
+    "h:mm:ss[<small>]A[</small>]"
+  );
+
+  let zurichElement = document.querySelector("#zurich");
+  let zurichDateElement = zurichElement.querySelector(".date");
+  let zurichTimeElement = zurichElement.querySelector(".time");
+  let zurichTime = moment().tz("Europe/Zurich");
+
+  zurichDateElement.innerHTML = zurichTime.format("MMMM Do YYYY");
+  zurichTimeElement.innerHTML = zurichTime.format(
+    "h:mm:ss [<small>]A[</small>]" //+ :SSS for milliseconds
+  );
+}
+updateTime();
+setInterval(updateTime, 1000); //1 if we had only minutes
